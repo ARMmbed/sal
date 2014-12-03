94eb118d86962409dc9861c9660c76877de734be
Brendan Moran <brendan.moran@arm.com>
2014-12-03 16:48:28 +0000

    (Untested) Tweaks to enable building Due to an oddity of the build system, two c files in the Socket directory had to be renamed to cpp Several modifications to types to enable the build Fixes for minor errors in main.cpp

--- socket_api.h
+++ socket_api.h
@@ -15,11 +15,11 @@ socket_error_t socket_init();
 socket_error_t socket_create(struct socket *socket, socket_proto_family_t family, socket_api_handler handler);
 socket_error_t socket_destroy(struct socket *socket);
 
-socket_error_t socket_connect(struct socket *socket, struct socket_addr *address, uint16_t port);
-socket_error_t socket_bind(struct socket *socket, struct socket_addr *address, uint16_t port);
+socket_error_t socket_connect(struct socket *socket, const struct socket_addr *address, const uint16_t port);
+socket_error_t socket_bind(struct socket *socket, const struct socket_addr *address, const uint16_t port);
 
-socket_error_t socket_start_listen(struct socket_addr *address, uint16_t port, socket_api_handler handler);
-socket_error_t socket_stop_listen(struct socket_addr *address, uint16_t port);
+socket_error_t socket_start_listen(const struct socket_addr *address, const uint16_t port, socket_api_handler handler);
+socket_error_t socket_stop_listen(const struct socket_addr *address, const uint16_t port);
 
 socket_error_t socket_start_send(struct socket *socket, void *arg, struct socket_buffer *buf, uint8_t autofree);
 socket_error_t socket_start_recv(struct socket *socket, void *arg);
