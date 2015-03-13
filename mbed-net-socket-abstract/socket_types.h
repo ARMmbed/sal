b7202eafada35339fe49ba9db64cb528edb86047
Brendan Moran <brendan.moran@arm.com>
2015-03-13 16:50:20 +0000

    Remove the handler_t define from mbed-net-socket-abstract

--- mbed-net-socket-abstract/socket_types.h
+++ mbed-net-socket-abstract/socket_types.h
@@ -91,26 +91,6 @@ typedef enum {
     SOCKET_BUFFER_NANOSTACK_PBUF,
 } socket_buffer_type_t;
 
-// TODO: The type of handler_t is TBD.
-// NOTE: Since handlers are passed using C++ references, a global null handler will be provided so that empty handlers
-// are provided for.  Overriding the null handler is likely to be useful for debugging.
-/*
- * std::function, in combination with std::bind appear to be the ideal solution for handler_t, however
- * std::bind, is not supported by all our targets. We might be able to work around this by porting
- * boost::bind to mbed
- */
-//typedef std::function<void(event_t*)> handler_t;
-/*
- * We already have a feature in mbed for handling function pointers.
- */
-//#include "FunctionPointer.h"
-//typedef FunctionPointer handler_t
-/*
- * Our fallback alternative for handler_t is a function pointer:
- */
-typedef void (*handler_t)(void *);
-
-
 struct socket_addr {
     socket_stack_t type;
 	uint32_t storage[4];
